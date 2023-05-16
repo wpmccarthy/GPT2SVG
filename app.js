fd52c8ea09dae7735b58b47e59065c5cf63f0dda
Will <mccarthywill@live.co.uk>
2023-05-16 12:16:47 -0700

    root path

--- app.js
+++ app.js
@@ -102,7 +102,7 @@ app.get('/*', (req, res) => {
 var serveFile = function (req, res) {
   var fileName = req.params[0];
   // console.log('\t File requested: ' + fileName);
-  return res.sendFile(fileName, { root: __dirname });
+  return res.sendFile(fileName, { root: __dirname + '/public/' });
 };
 
 // Handle the POST request from form
